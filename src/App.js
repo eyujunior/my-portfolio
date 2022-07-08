814b5b59ed9c6e62ecfbb3b4e81cd86e12b26c6a
eyujunior <eyujunior052@gmail.com>
2022-07-08 11:11:18 +0300

    added tailwind/forms for contact page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
 function App() {
   // const handleThemeChange = () => {}
   return (
     <>
       <Router>
-        <div className='py-2 px-6'>
+        <div className="py-2 px-3 xl:px-6">
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/*' element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </div>
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
-      sans: ['poppins', 'system-ui'],
-      title: ['montserrat', 'Georgia'],
+      sans: ["poppins", "system-ui"],
+      title: ["montserrat", "Georgia"],
     },
-    colors: {
-      primary: 'rgb(114,76,249)',
-      secondary: 'rgb(217,3,104)',
-      dark: 'rgb(12,10,20)',
-      tagLight: 'rgb(227,227,227)',
-      tagDark: 'rgb(39,28,83)',
-      text: 'rgb(20,20,0)',
-      white: 'rgb(255,255,255)',
+
+    extend: {
+      colors: {
+        primary: "rgb(114,76,249)",
+        secondary: "rgb(217,3,104)",
+        dark: "rgb(12,10,20)",
+        tagLight: "rgb(227,227,227)",
+        tagDark: "rgb(39,28,83)",
+        text: "rgb(20,20,0)",
+        white: "rgb(255,255,255)",
+      },
     },
-    extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
